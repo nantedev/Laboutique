21b24c8a8d5f1944b1fdb180fe53413d3fa10288
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-02-21 14:14:26 +0400

    ✨  E01: add mode theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from "react";
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,12 +22,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+      setMounted(true);
+      }, []);
+  if (!mounted) {
+  return null;
+  }
   return (
     <html lang="fr" suppressHydrationWarning>
       <body
         className={`${inter.className}`}
       >
+        <ThemeProvider
+        attribute='class'
+        defaultTheme='light'
+        enableSystem
+        disableTransitionOnChange
+      >
         {children}
+      </ThemeProvider>
       </body>
     </html>
   );
