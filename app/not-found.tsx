f53168782131eaf25249149f01c9394b6d4c8fa9
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-02-22 19:32:49 +0400

    🐛  E02: fix es lint ts error with &apos;

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 'use client';
 import { APP_NAME } from '@/lib/constants';
 import Image from 'next/image';
@@ -16,13 +15,13 @@ const NotFound = () => {
       />
       <div className='p-6 rounded-lg shadow-md w-1/3 text-center'>
         <h1 className='text-3xl font-bold mb-4'>Non trouvée</h1>
-        <p className='text-destructive'>Je n'arrive pas à trouver la page demandée</p>
+        <p className='text-destructive'>Je n&apos;arrive pas à trouver la page demandée</p>
         <Button
           variant='outline'
           className='mt-4 ml-2'
           onClick={() => (window.location.href = '/')}
         >
-          Revenir à l'accueil
+          Revenir à l&apos;accueil
         </Button>
       </div>
     </div>
