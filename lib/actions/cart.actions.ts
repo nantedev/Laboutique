98fe9d555301206aa7454afffbb9dea72ace44a0
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-02-23 16:09:58 +0400

    🗃️ E04: handle multiple item and quantity

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -68,6 +68,44 @@ export const addItemToCart = async (data: z.infer<typeof cartItemSchema>) => {
         success: true,
         message: 'Item added to cart successfully',
       };
+    } else {
+      // Check for existing item in cart
+      const existItem = (cart.items as CartItem[]).find(
+        (x) => x.productId === item.productId
+      );
+      // If not enough stock, throw error
+      if (existItem) {
+        if (product.stock < existItem.qty + 1) {
+          throw new Error('Not enough stock');
+        }
+    
+        // Increase quantity of existing item
+        (cart.items as CartItem[]).find(
+          (x) => x.productId === item.productId
+        )!.qty = existItem.qty + 1;
+      } else {
+        // If stock, add item to cart
+        if (product.stock < 1) throw new Error('Not enough stock');
+        cart.items.push(item);
+      }
+    
+      // Save to database
+      await prisma.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: cart.items as Prisma.CartUpdateitemsInput[],
+          ...calcPrice(cart.items as CartItem[]),
+        },
+      });
+    
+      revalidatePath(`/product/${product.slug}`);
+    
+      return {
+        success: true,
+        message: `${product.name} ${
+          existItem ? 'updated in' : 'added to'
+        } cart successfully`,
+      };
     }
    
   } catch (error) {
