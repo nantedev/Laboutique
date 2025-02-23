db427fd34c312564a1589e814e42d02d76760e7c
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-02-23 22:15:34 +0400

    💄  E04: add animation on button plus, add, minus click (useTransition)

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,15 +1,19 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
-import { Plus, Minus } from 'lucide-react';
+import { Plus, Minus, Loader } from 'lucide-react';
 import { toast } from 'sonner'; // Importer depuis Sonner
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import { Cart, CartItem } from '@/types';
+import { useTransition } from 'react';
 
 const AddToCart = ({cart, item }: { cart?: Cart; item: CartItem; }) => {
+
+  const [isPending, startTransition] = useTransition();
   const router = useRouter();
 
   const handleAddToCart = async () => {
+    startTransition(async () => {
     // Execute the addItemToCart action
     const res = await addItemToCart(item);
 
@@ -26,37 +30,53 @@ const AddToCart = ({cart, item }: { cart?: Cart; item: CartItem; }) => {
         onClick: () => router.push('/cart'),
       },
     });
+  });
   };
-
   // Remove item from cart
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
-  
-    if (res.success) {
-      toast.success(res.message);
-    } else {
-      toast.error(res.message);
-    }
-    return;
-  };
+    startTransition(async () => {
+
+        const res = await removeItemFromCart(item.productId);
+      
+        if (res.success) {
+          toast.success(res.message);
+        } else {
+          toast.error(res.message);
+        }
+        return;
+      }
+    )
+  }
     //Check if item is in cart
   
     const existItem = cart && cart.items.find((x) => x.productId === item.productId);
   
     return existItem ? (
     <div>
-      <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
-        <Minus className='w-4 h-4' />
+      <Button type='button'  variant='outline' disabled={isPending} onClick={handleRemoveFromCart}>
+        {isPending ? (
+          <Loader className='w-4 h-4' animate-spin/>  
+          ):( 
+            <Minus className='w-4 h-4' /> 
+          )}
       </Button>
       <span className='px-2'>{existItem.qty}</span>
-      <Button type='button' variant='outline' onClick={handleAddToCart}>
-        <Plus className='w-4 h-4' />
+      <Button type='button' variant='outline' onClick={handleAddToCart} disabled={isPending}>
+        {isPending ? (
+          <Loader className='w-4 h-4' animate-spin/> 
+        ):(
+          <Plus className='w-4 h-4' />
+        )}
       </Button>
     </div>
   ) : (
     <Button className='w-full' type='button' onClick={handleAddToCart}>
-      <Plus className='w-4 h-4' />
-      Add to cart
+      {isPending ? (
+          <Loader className='w-4 h-4' animate-spin/> 
+        ):(
+          <Plus className='w-4 h-4' />
+        )}
+      Ajouter au panier
     </Button>
   );
   };
