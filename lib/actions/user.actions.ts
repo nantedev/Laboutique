ac6bd26e2941c6655e6a6e802fa2066d124f5180
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-02-27 16:21:06 +0400

    ✨  E08: lib/actions/user.actions.ts > create user profile action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -140,3 +140,33 @@ export async function updateUserPaymentMethod(
     return { success: false, message: formatError(error) };
   }
 }
+
+export async function updateProfile(user: { name: string; email: string }) {
+  try {
+    const session = await auth();
+
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        id: session?.user?.id,
+      },
+    });
+
+    if (!currentUser) throw new Error('Utilisateur non trouvé');
+
+    await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name: user.name,
+      },
+    });
+
+    return {
+      success: true,
+      message: 'Utilisateur mis à jour',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
