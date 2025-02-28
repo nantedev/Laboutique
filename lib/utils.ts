2a24a57b5f1b3ce81771f7d00ad171079e129777
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-02-28 23:23:35 +0400

    ✨ E09 : add utility that format a number to a string with commas

--- lib/utils.ts
+++ lib/utils.ts
@@ -132,3 +132,8 @@ export function formUrlQuery({
   );
 }
 
+//  Format Numbers
+const NUMBER_FORMATTER = new Intl.NumberFormat('en-US');
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number);
+}
