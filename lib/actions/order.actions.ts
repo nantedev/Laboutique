15a80397e3fdf7240524ccd936b215e4b81b3b4e
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-03-01 00:55:31 +0400

    ✨ E09: Delete Order (Admin) > create action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -374,3 +374,18 @@ export async function getAllOrders({
 }
 
 
+// Delete Order
+export async function deleteOrder(id: string) {
+  try {
+    await prisma.order.delete({ where: { id } });
+
+    revalidatePath('/admin/orders');
+
+    return {
+      success: true,
+      message: 'Order deleted successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
