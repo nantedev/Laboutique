2e6ce229099d6bed4010014b6a0a9d5421d485d8
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-03-01 12:21:17 +0400

    ✨ E10: product.action > create getAllProducts function

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 import { convertToPlainObject } from '../utils';
-import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '../constants';
 import { prisma } from '@/db/prisma';
 
 // Get the latest products
@@ -20,3 +20,28 @@ export async function getProductBySlug(slug: string) {
   });
 }
 
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
