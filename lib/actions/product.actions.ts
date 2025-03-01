d74e1e6f5d9e29b5dd5c9ae6cc019a954a2cd599
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-03-01 15:32:02 +0400

    🚧 E10: admin/product : in product.actions, create Delete Products From Admin Area

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,8 @@
 'use server';
-import { convertToPlainObject } from '../utils';
+import { convertToPlainObject, formatError } from '../utils';
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '../constants';
 import { prisma } from '@/db/prisma';
+import { revalidatePath } from 'next/cache';
 
 // Get the latest products
 export async function getLatestProducts() {
@@ -44,4 +45,27 @@ export async function getAllProducts({
     data,
     totalPages: Math.ceil(dataCount / limit),
   };
+}
+
+// Delete Product
+export async function deleteProduct(id: string) {
+  try {
+    const productExists = await prisma.product.findFirst({
+      where: { id },
+    });
+
+    if (!productExists) throw new Error('Product not found');
+
+    await prisma.product.delete({ where: { id } });
+
+      // ⚡ Forcer la mise à jour de la page
+    revalidatePath('/admin/products');
+
+    return {
+      success: true,
+      message: 'Product deleted successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
 }
