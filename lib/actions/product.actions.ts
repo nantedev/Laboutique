b1596eb51b6924da224f1a1b94440b41ec24b414
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-03-01 16:27:33 +0400

    🚧 E10: admin/product : product.actions, add create / update product

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,6 +3,8 @@ import { convertToPlainObject, formatError } from '../utils';
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '../constants';
 import { prisma } from '@/db/prisma';
 import { revalidatePath } from 'next/cache';
+import { insertProductSchema, updateProductSchema } from '../validator';
+import { z } from 'zod';
 
 // Get the latest products
 export async function getLatestProducts() {
@@ -64,6 +66,50 @@ export async function deleteProduct(id: string) {
     return {
       success: true,
       message: 'Product deleted successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+
+// Create Product
+export async function createProduct(data: z.infer<typeof insertProductSchema>) {
+  try {
+    // Validate and create product
+    const product = insertProductSchema.parse(data);
+    await prisma.product.create({ data: product });
+
+    revalidatePath('/admin/products');
+
+    return {
+      success: true,
+      message: 'Product created successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update Product
+export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
+  try {
+    // Validate and find product
+    const product = updateProductSchema.parse(data);
+    const productExists = await prisma.product.findFirst({
+      where: { id: product.id },
+    });
+
+    if (!productExists) throw new Error('Product not found');
+
+    // Update product
+    await prisma.product.update({ where: { id: product.id }, data: product });
+
+    revalidatePath('/admin/products');
+
+    return {
+      success: true,
+      message: 'Product updated successfully',
     };
   } catch (error) {
     return { success: false, message: formatError(error) };
