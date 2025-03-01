7b9a794f90397d659238d5551b8f4454c1bd09c6
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-03-01 11:35:35 +0400

    ✨ E10: admin/product > create page

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,22 @@
+import { requireAdmin } from "@/lib/auth-guard";
+
+const AdminProductsPage = async (props: {searchParams: Promise<{
+    page: string;
+    query: string;
+    category: string;
+}>}) => {
+    await requireAdmin();
+    const searchParams = await props.searchParams;
+    const page = Number(searchParams.page) || 1;
+    const searchText = searchParams.query || '';
+    const category = searchParams.category || '';
+    return (
+      <div className='space-y-2'>
+        <div className='flex-between'>
+          <h1 className='h2-bold'>Produits</h1>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AdminProductsPage;
