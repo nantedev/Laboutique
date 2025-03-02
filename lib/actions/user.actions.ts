14401b4194b8d586bc06c10a7a03ad0fe09a393e
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-03-02 08:28:48 +0400

    ✨ E11: admin, user.actions > Get display all users

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -13,6 +13,8 @@ import { hashSync } from 'bcrypt-ts-edge';
 import { prisma } from '@/db/prisma';
 import { formatError } from '../utils';
 import { ShippingAddress } from '@/types';
+import { revalidatePath } from 'next/cache';
+import { PAGE_SIZE } from '../constants';
 
 export async function signInWithCredentials(
     prevState: unknown,
@@ -162,6 +164,8 @@ export async function updateProfile(user: { name: string; email: string }) {
       },
     });
 
+    revalidatePath('/user/profile-form')
+
     return {
       success: true,
       message: 'Utilisateur mis à jour',
@@ -169,4 +173,26 @@ export async function updateProfile(user: { name: string; email: string }) {
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
+}
+
+// Get all users
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const data = await prisma.user.findMany({
+    orderBy: { createdAt: 'desc' },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.user.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
