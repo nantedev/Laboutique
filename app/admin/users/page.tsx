bf8aad442b3935be9415874ff1004b1434e24d6e
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-03-02 17:15:45 +0400

    ✨ E11 : admin/orders > Add a new `query` param to the `SearchParams` prop

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -20,17 +20,30 @@ import { getAllUsers, deleteUser } from '@/lib/actions/user.actions';
 const AdminUserPage = async (props: {
     searchParams: Promise<{
       page: string;
+      query: string;
     }>;
   }) => {
     const searchParams = await props.searchParams;
   
-    const { page = '1' } = searchParams;
+    const { page = '1', query: searchText } = searchParams;
   
-    const users = await getAllUsers({ page: Number(page) });
+    const users = await getAllUsers({ page: Number(page), query: searchText});
   
     return (
         <div className='space-y-2'>
-          <h1 className='h2-bold'>Utilisateurs</h1>
+          <div className='flex items-center gap-3'>
+                <h1 className='h2-bold'>Utilisateurs</h1>
+                {searchText && (
+                  <div>
+                    Filtrés par <i>&quot;{searchText}&quot;</i>{' '}
+                    <Link href={`/admin/users`}>
+                      <Button variant='outline' size='sm'>
+                        Supprimer le filtre
+                      </Button>
+                    </Link>
+                  </div>
+                )}
+              </div>
           <div>
             <Table>
               <TableHeader>
