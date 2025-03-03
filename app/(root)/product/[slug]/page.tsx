e168689279a7b4320cddd87cc52a004b73ff2325
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-03-03 20:12:34 +0400

    ✨ E13: add rating to product

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -10,6 +10,8 @@ import { getMyCart } from '@/lib/actions/cart.actions';
 import { round2 } from '@/lib/utils';
 import { auth } from '@/auth';
 import ReviewList from './review-list';
+import Rating from '@/components/shared/product/rating';
+
 
 
 const ProductDetailsPage = async (props: {
@@ -42,10 +44,8 @@ const ProductDetailsPage = async (props: {
                 {product.brand} {product.category}
               </p>
               <h1 className='h3-bold'>{product.name}</h1>
-              <p>
-                {product.rating} sur {product.numReviews} reviews
-              </p>
-
+                  <Rating value={Number(product.rating)} />
+                  <p>{product.numReviews} reviews</p>
                <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
                   <ProductPrice
                     value={Number(product.price)}
