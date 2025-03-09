dc6c9d5f94eaa2e5b2716fe825ebbae2598671d9
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-03-09 22:38:31 +0400

    🐛 E16: fix hydratation pb ?

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,14 @@
+'use client'
+
 import { APP_NAME } from '@/lib/constants';
+import { useState, useEffect } from 'react';
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
+  const [currentYear, setCurrentYear] = useState('');
+
+  useEffect(() => {
+    setCurrentYear(new Date().getFullYear().toString());
+  }, []);
 
   return (
     <footer className='border-t'>
